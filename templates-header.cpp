2876f9a4f8d153f267f8b35f8623606af0aee11d
rainy2k <xinyuezhang@mail.ustc.edu.cn>
2023-04-20 20:28:46 +0800

    first commit

---
+++ templates-header.cpp
@@ -0,0 +1,22 @@
+#include <iostream>  //使用cin,cout的头文件
+#include <cstdio>    //使用scanf和printf的头文件
+#include <cstring>   //使用C风格字符串函数的头文件
+#include <algorithm> //使用算法库的头文件，max,min,swap,sort等
+#include <string>    //使用string时的头文件
+
+#include <vector>    //使用vector时的头文件 : stl
+#include <stack>     //使用stack时的头文件 : stl
+#include <set>       //使用set的头文件: stl
+#include <list>      //使用list的头文件
+#include <deque>     //使用deque的头文件 双端队列
+#include <map>       //使用map的头文件
+#include <queue>
+
+#include <bitset>    //使用bitset时的头文件
+#include <cstdlib>   //使用memset函数的头文件
+#include <cmath>     //数学函数的头文件
+using namespace std;
+
+int main() {
+
+}
